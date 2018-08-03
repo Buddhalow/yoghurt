78bf41519d3d93a7138e7a04c32140997db279f4
Alexander Forselius <alexander.forselius@buddhalow.com>
2018-08-03 20:16:01 +0200

    Added window sizing

--- docs/example/window.js
+++ docs/example/window.js
@@ -3,5 +3,6 @@ var rect = {
     width: window.innerWidth,
     height: window.innerHeight
 }
+let canvas = document.querySelector('canvas')
 canvas.width = rect.height;
 canvas.height = rect.height;
